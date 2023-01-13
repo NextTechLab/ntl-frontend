d5a76f9bdf6b98a99c6319fd0521b95464bd928e
vishal <vishaltyagi.dev098@gmail.com>
2023-01-14 00:35:45 +0530

    title

---
+++ components/Sublabs/Syndicates.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Image from 'next/image'
+import myImage from '../../public/image.png'
+
+function Syndicates() {
+  return (
+    <div className='flex justify-center items-center md:w-[30%] w-[90%] mx-2 my-5'>
+      <div className="">
+        <Image src={myImage} alt="image"/>
+      </div>
+      <div className="">
+        <div className="">John Doe</div>
+        <div className="">Domain name</div>
+        <div className="">Department name</div>
+        <div className="">year of study</div>
+      </div>
+    </div>
+  )
+}
+
+export default Syndicates
---
+++ components/Sublabs/Title.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Title() {
+  return (
+    <div className='bg-[#FFFFF0] text-[#29D9FE] w-full flex justify-center items-center text-3xl font-bold'>
+      Title
+    </div>
+  )
+}
+
+export default Title
